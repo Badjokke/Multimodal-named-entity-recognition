082ed524efa18b49ccc9d3d9b1ca423e50b17027
jtrefil <jirka.trefilu@gmail.com>
2024-10-30 23:54:29 +0100

    #4 line streaming added

--- src/async_io/filesystem.py
+++ src/async_io/filesystem.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+from typing import Callable, Coroutine
+
 import aiofiles
 
 
@@ -8,13 +10,38 @@ async def load_file(path: str):
         return await f.read()
 
 
+async def file_lines_generator(path: str):
+    async with aiofiles.open(path, 'r') as f:
+        async for line in f:
+            yield line
+    yield "EOF"
+    return
+
+
 async def save_file(path: str, content: bytes):
     async with aiofiles.open(path, 'wb') as f:
         await f.write(content)
 
 
-async def load_directory_contents(path: str, queue: asyncio.Queue):
+async def __load_directory_contents(path, queue: asyncio.Queue, file_parser: Callable[[str], Coroutine]):
     children = os.listdir(path)
     for i in range(0, len(children)):
-        await queue.put((children[i], await load_file(os.path.join(path, children[i]))))
+        await queue.put((children[i], await file_parser(os.path.join(path, children[i]))))
+    await queue.put(None)
+
+
+async def load_directory_contents(path: str, queue: asyncio.Queue):
+    await __load_directory_contents(path, queue, load_file)
+
+
+async def load_directory_contents_generator(path: str, queue: asyncio.Queue):
+    children = os.listdir(path)
+    for i in range(0, len(children)):
+        gen = file_lines_generator(os.path.join(path, children[i]))
+        while True:
+            elem = await gen.__anext__()
+            await queue.put(("line", elem))
+            if elem == "EOF":
+                break
+        await queue.put((children[i], "EOF"))
     await queue.put(None)
