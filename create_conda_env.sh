51397b490154b01657008cfc3cc639e346dd489e
jtrefil <jirka.trefilu@gmail.com>
2025-01-03 01:09:16 +0100

    #32 conda env build

---
+++ create_conda_env.sh
@@ -0,0 +1,5 @@
+declare python_version = 3.10
+declare env_name = "multimodal_named_entity"
+conda create -n $env_name python=$python_version
+conda activate multimodal_named_entity
+pip3 install -r requirements.txt
