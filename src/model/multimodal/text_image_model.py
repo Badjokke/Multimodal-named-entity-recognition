269c2d5ed00db3fffe6e91ae044906d7347b492a
jtrefil <jirka.trefilu@gmail.com>
2025-02-15 22:15:26 +0100

    bilstm bert crf model

--- src/model/multimodal/text_image_model.py
+++ src/model/multimodal/text_image_model.py
@@ -1,51 +1,59 @@
 import torch
-from model.multimodal.Expert import Expert
-from model.multimodal.NoisyTopkRouter import NoisyTopkRouter
-import torch.nn.functional as F
-from TorchCRF import CRF
+
+from torchcrf import CRF
+
+
 class CombinedModel(torch.nn.Module):
-    def __init__(self, visual_model, text_model, num_labels, num_experts=5):
+    def __init__(self, visual_model, text_model, num_labels, num_experts=3):
         super().__init__()
         self.visual_model = visual_model
         self.text_model = text_model
-        self.n_embeds = 1024
 
-        self.__out_layer = torch.nn.Linear(768, num_labels)
-
-        #self.__text_lin_layer = torch.nn.Linear(text_model.config.hidden_size, self.n_embeds)
-        #self.text_experts = torch.nn.ModuleList([Expert(self.n_embeds) for _ in range(num_experts)])
-        #self.visual_experts = torch.nn.ModuleList([Expert(visual_model.output_size) for _ in range(num_experts)])
-        #self.text_gate = NoisyTopkRouter(self.n_embeds, num_experts, 3)
-        #self.visual_gate = NoisyTopkRouter(visual_model.output_size, num_experts, 3)
-
+        self.__out_layer = torch.nn.Linear(text_model.config.hidden_size, num_labels)
         self.__fusion_layer = torch.nn.Sequential(
-            torch.nn.Linear(visual_model.output_size + self.n_embeds, 768),
+            torch.nn.Linear(self.visual_model.output_size + text_model.config.hidden_size,
+                            text_model.config.hidden_size),
+            torch.nn.LayerNorm(text_model.config.hidden_size),
             torch.nn.GELU(),
-            torch.nn.Dropout(0.2),
+            torch.nn.Dropout(0.3),
+        )
+        """
+        self.__projection_layer = torch.nn.Linear(self.visual_model.output_size, text_model.config.hidden_size)
+
+        self.text_experts = torch.nn.ModuleList([Expert(text_model.config.hidden_size) for _ in range(num_experts)])
+        self.visual_experts = torch.nn.ModuleList([Expert(visual_model.output_size) for _ in range(num_experts)])
+        self.text_gate = NoisyTopkRouter(text_model.config.hidden_size, num_experts, 3)
+        self.visual_gate = NoisyTopkRouter(visual_model.output_size, num_experts, 3)
+        """
+
+        self.bilstm = torch.nn.LSTM(
+            text_model.config.hidden_size,
+            text_model.config.hidden_size // 2,
+            num_layers=1,
+            bidirectional=True,
+            batch_first=True,
         )
         self.crf = CRF(num_labels, batch_first=True)
 
     def forward(self, visual_feats, text_feats):
         visual_out = self.visual_model(visual_feats)
-        text_out = self.text_model(**text_feats).last_hidden_state  #  (sentence_len, hidden_size)
+        text_out = self.text_model(**text_feats).last_hidden_state  # (sentence_len, hidden_size)
 
-        visual_out = F.normalize(visual_out, p=2, dim=-1)
-        text_out = F.normalize(text_out, p=2, dim=-1)
+        # text_out = self.__expert_pass(text_out, self.text_experts, self.text_gate)
+        # visual_out = self.__expert_pass(visual_out, self.visual_experts, self.visual_gate)
 
         text_out = text_out.repeat(visual_out.size(0), 1, 1)
-        
-        #text_out = self.__text_lin_layer(text_out)
-        #text_out = self.__expert_pass(text_out, self.text_experts, self.text_gate)
-        #visual_out = self.__expert_pass(visual_out, self.visual_experts, self.visual_gate)
-
         visual_out = visual_out.unsqueeze(1).expand(-1, text_out.size(1), -1)
-        combined = self.__fusion_layer(torch.cat([text_out, visual_out], dim=-1))
 
-        x = self.__out_layer(combined)
-        return x
+        combined = self.__fusion_layer(torch.cat([visual_out, text_out], dim=-1))
+        combined, _ = self.bilstm(combined)
+        return self.__out_layer(combined)
 
-    def crf_pass(self, x,y, mask):
-        return -self.crf(x, y, mask)
+    def crf_pass(self, x, y, mask, weight):
+        base_loss = -self.crf(x, y, mask)
+        weight_mask = weight[y]
+        weighted_loss = base_loss * weight_mask
+        return weighted_loss.mean()
 
     def crf_decode(self, x, mask):
         return self.crf.decode(x, mask)
--- src/train/train.py
+++ src/train/train.py
@@ -3,21 +3,28 @@ from typing import Iterable, Union
 import numpy as np
 import torch
 from peft import PeftModel
+from seqeval.metrics import classification_report
 from sklearn.utils.class_weight import compute_class_weight
-from metrics.metrics import  Metrics
+from transformers import get_linear_schedule_with_warmup
+
+from metrics.metrics import Metrics
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def _compute_inverse_freq_weights(y, alpha=0.5) -> torch.Tensor:
     weights = [1 / (np.bincount(x) ** alpha) for x in y]
     return torch.Tensor(weights).to(torch.float32).to(device)
 
+
 def _compute_class_weights_rare_events(y) -> torch.Tensor:
     weights = compute_class_weight(class_weight="balanced", classes=np.unique(y), y=y)
     return torch.from_numpy(weights).to(torch.float32).to(device)
 
+
 def _create_cross_entropy_loss_criterion(y) -> torch.nn.CrossEntropyLoss:
-    return torch.nn.CrossEntropyLoss(ignore_index=-100, weight=_compute_class_weights_rare_events(y))
+    return torch.nn.CrossEntropyLoss(ignore_index=-100)
+
 
 def align_labels(word_ids, labels):
     padded_labels = []
@@ -25,14 +32,63 @@ def align_labels(word_ids, labels):
         padded_labels.append(-100 if word is None else labels[word])
     return padded_labels
 
+
 def _create_optimizer(parameters: Iterable[torch.Tensor], learning_rate=1e-5, momentum=0.9) -> torch.optim.Optimizer:
     return torch.optim.SGD(parameters, lr=learning_rate, momentum=momentum)
 
-def _create_adamw_optimizer(parameters: Iterable[torch.Tensor], learning_rate=2e-5) -> torch.optim.AdamW:
-    return torch.optim.AdamW(parameters, lr=learning_rate, weight_decay=0.01)
 
-def _create_scheduler(optimizer, t_max) -> torch.optim.lr_scheduler.CosineAnnealingLR:
-    return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=t_max, verbose=True)
+def _create_adamw_optimizer(model, learning_rate=1e-5) -> torch.optim.AdamW:
+    no_decay = ['bias', 'LayerNorm.weight']
+    bert_params = [(n, p) for n, p in model.text_model.named_parameters()]
+    vit_params = [(n, p) for n, p in model.visual_model.named_parameters()]
+    other_params = [(n, p) for n, p in model.named_parameters() if
+                    not any(x in n for x in ['text_model', 'visual_model'])]
+
+    optimizer_grouped_parameters = [
+        # BERT parameters
+        {
+            'params': [p for n, p in bert_params if not any(nd in n for nd in no_decay)],
+            'weight_decay': 0.01,
+            'lr': 2e-5
+        },
+        {
+            'params': [p for n, p in bert_params if any(nd in n for nd in no_decay)],
+            'weight_decay': 0.0,
+            'lr': 2e-5
+        },
+        # ViT parameters
+        {
+            'params': [p for n, p in vit_params if not any(nd in n for nd in no_decay)],
+            'weight_decay': 0.01,
+            'lr': 2e-5
+        },
+        {
+            'params': [p for n, p in vit_params if any(nd in n for nd in no_decay)],
+            'weight_decay': 0.0,
+            'lr': 2e-5
+        },
+        # Other parameters (fusion, CRF etc)
+        {
+            'params': [p for n, p in other_params if not any(nd in n for nd in no_decay)],
+            'weight_decay': 0.01,
+            'lr': 1e-4  # Higher learning rate for new parameters
+        },
+        {
+            'params': [p for n, p in other_params if any(nd in n for nd in no_decay)],
+            'weight_decay': 0.0,
+            'lr': 1e-4
+        }
+    ]
+    return torch.optim.AdamW(optimizer_grouped_parameters, weight_decay=0.01, eps=1e-8, betas=(0.9, 0.999))
+
+
+def _create_scheduler(optimizer, training_steps,) -> torch.optim.lr_scheduler.CosineAnnealingLR:
+    return get_linear_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps=training_steps//10,
+    num_training_steps=training_steps
+)
+
 
 def training_loop_text_only(model: Union[torch.nn.Module, PeftModel], train_data, validation_data, tokenizer, epochs=10,
                             patience=3):
@@ -52,25 +108,27 @@ def training_loop_text_only(model: Union[torch.nn.Module, PeftModel], train_data
     print(f"Average loss: {mean_loss / epochs}")
     return model
 
+
 def perform_epoch_text_only(model, tokenizer, train_data, loss_criterion, optimizer, scheduler):
     running_loss = 0.0
     y_pred, y_true = [], []
     for i in range(len(train_data)):
         data_sample = train_data[i]
-        text, labels = tokenizer(data_sample[0], return_tensors="pt", is_split_into_words=True), data_sample[1].to(device)
+        text, labels = tokenizer(data_sample[0], return_tensors="pt", is_split_into_words=True), data_sample[1].to(
+            device)
         word_ids = text.word_ids()
         text = {key: value.to(device) for key, value in text.items()}
         aligned_labels = torch.tensor(align_labels(word_ids, labels), device=device, dtype=torch.long).unsqueeze(0)
 
         optimizer.zero_grad()
 
-        outputs, loss = model(text,aligned_labels)
-        #loss = loss_criterion(outputs.squeeze(0), aligned_labels)
+        outputs, loss = model(text, aligned_labels)
+        # loss = loss_criterion(outputs.squeeze(0), aligned_labels)
         loss.backward()
         running_loss += loss.item()
 
         optimizer.step()
-        #scheduler.step()
+        # scheduler.step()
         y_pred.append(decode_labels_majority_vote(word_ids[1:], torch.argmax(outputs, dim=2)[0:, 1:]).tolist())
         y_true.append((labels.unsqueeze(0).repeat(outputs.size(0), 1)).tolist())
 
@@ -78,26 +136,39 @@ def perform_epoch_text_only(model, tokenizer, train_data, loss_criterion, optimi
     macro_f1 = metrics.macro_f1(metrics.confusion_matrix())
     return running_loss / len(train_data), macro_f1
 
+
 def training_loop_combined(model: Union[torch.nn.Module, PeftModel], train_data, validation_data, test_data, tokenizer,
-                           class_occurrences, epochs=10, patience=3):
+                           class_occurrences, labels, epochs=10, patience=3):
     model.to(device)
     loss_criterion = _create_cross_entropy_loss_criterion(class_occurrences)
-    optimizer = _create_adamw_optimizer(model.parameters())
-    scheduler = _create_scheduler(optimizer, t_max=epochs * len(train_data))
-    #optimizer, scheduler = setup_optimizer(model, t_max=epochs*len(train_data))
+    optimizer = _create_adamw_optimizer(model)
+    scheduler = _create_scheduler(optimizer, epochs * len(train_data))
+    w = _compute_class_weights_rare_events(class_occurrences)
+    # optimizer, scheduler = setup_optimizer(model, t_max=epochs*len(train_data))
     for epoch in range(epochs):
-        training_loss = perform_epoch(model, tokenizer, train_data, loss_criterion, optimizer, scheduler)
-        val_loss = validate_after_epoch(model, tokenizer, loss_criterion, validation_data)
-        test_results = validate_after_epoch(model, tokenizer, loss_criterion, test_data)
+        #print(f"==EPOCH {epoch}==")
+        # val_loss = validate_after_epoch(model, tokenizer, loss_criterion, validation_data, {value:key for key,value in labels.items()})
+        #print("==TRAIN==")
+        training_loss = perform_epoch(model, tokenizer, train_data, loss_criterion, optimizer, scheduler,
+                                      {value: key for key, value in labels.items()}, w)
+        #print("==VAL==")
+        val_loss = validate_after_epoch(model, tokenizer, loss_criterion, validation_data,
+                                        {value: key for key, value in labels.items()}, w)
+        #print("==TEST==")
+        test_results = validate_after_epoch(model, tokenizer, loss_criterion, test_data,
+                                            {value: key for key, value in labels.items()}, w)
         print(f"[epoch: {epoch + 1}] Training loss: {training_loss[0]}. Training macro f1: {training_loss[1]['macro']}; micro f1: {training_loss[1]['micro']}, acc: {training_loss[1]['accuracy']}")
         print(f"[epoch: {epoch + 1}] Validation loss: {val_loss[0]}. Validation macro f1: {val_loss[1]['macro']}; micro f1: {val_loss[1]['micro']}, acc: {val_loss[1]['accuracy']}")
         print(f"[epoch: {epoch + 1}] Test loss: {test_results[0]}. Test macro f1: {test_results[1]['macro']}; micro f1: {test_results[1]['micro']}, acc: {test_results[1]['accuracy']}")
+        print()
     return model
+
 
 def early_stop():
     print("Implement me!")
 
-def perform_epoch(model, tokenizer, train_data, loss_criterion, optimizer, scheduler):
+
+def perform_epoch(model, tokenizer, train_data, loss_criterion, optimizer, scheduler, labels_mapping, w):
     model.train()
     running_loss = 0.0
     y_pred = []
@@ -113,37 +184,43 @@ def perform_epoch(model, tokenizer, train_data, loss_criterion, optimizer, sched
 
         optimizer.zero_grad()
 
-        aligned_labels = torch.tensor(align_labels(word_ids, labels), device=device, dtype=torch.long).unsqueeze(0).repeat(images.size(0),1)
-        #aligned_labels = torch.tensor(align_labels(word_ids, labels), device=device, dtype=torch.long).unsqueeze(0)
+        aligned_labels = torch.tensor(align_labels(word_ids, labels), device=device, dtype=torch.long).unsqueeze(0).repeat(images.size(0), 1)
         outputs = model(images, text)
-        aligned_labels = aligned_labels[0:, 1:]
-        outputs = outputs[0:, 1:]
-        mask = torch.ones_like(aligned_labels,device=device).bool()
-        loss = model.crf_pass(outputs, aligned_labels, mask)
 
-        #loss = loss_criterion(outputs.permute(0,2,1), aligned_labels)
+        aligned_labels = aligned_labels[0:, 1:-1]
+        outputs = outputs[0:, 1:-1]
+        mask = torch.ones_like(aligned_labels, device=device).bool()
+
+        loss = model.crf_pass(outputs, aligned_labels, mask, w)#loss_criterion(torch.permute(outputs, (0, 2, 1)), aligned_labels)
         running_loss += loss.item()
-        #y_pred.append(decode_labels_majority_vote(word_ids[1:], torch.argmax(outputs.squeeze(0),dim=1)[1:]))
-        #y_true.append(labels)
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         loss.backward()
-        #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)
         optimizer.step()
         scheduler.step()
-
         y_pred.append(model.crf_decode(outputs, mask))
-        #y_pred.append(torch.argmax(outputs, dim=-1).tolist())
         y_true.append(aligned_labels.tolist())
-
-    metrics = Metrics(y_pred, y_true, 9, {_:str(_) for _ in range(9)})
+        """
+        for prediction in list(
+                map(lambda x: list(map(lambda y: labels_mapping[int(y)], x)), model.crf_decode(outputs,mask))):
+            y_pred.append(prediction)
+        for y_tr in list(map(lambda batch: list(map(lambda item: labels_mapping[int(item)], batch)), aligned_labels)):
+            y_true.append(y_tr)
+        """
+    metrics = Metrics(y_pred, y_true, len(labels_mapping.keys()), labels_mapping)
     macro_f1 = metrics.macro_f1(metrics.confusion_matrix())
     micro_f1 = metrics.micro_f1(metrics.confusion_matrix())
     acc = metrics.accuracy()
+
+    # print("===TRAIN===")
+    # print(classification_report(y_true, y_pred))
     return running_loss / len(train_data), {"macro": macro_f1, "micro": micro_f1, "accuracy": acc}
 
-def validate_after_epoch(model, tokenizer, loss_criterion, validation_data) -> tuple[
-    float, dict[str,float]]:
+
+def validate_after_epoch(model, tokenizer, loss_criterion, validation_data, labels_mapping, w) -> tuple[
+    float, dict[str, float]]:
     model.eval()
-    loss = 0.
+    running_loss = 0.
     y_true, y_pred = [], []
     with torch.no_grad():
         for i in range(len(validation_data)):
@@ -154,26 +231,40 @@ def validate_after_epoch(model, tokenizer, loss_criterion, validation_data) -> t
                                                                                                      is_split_into_words=True)
             word_ids = text.word_ids()
 
-            aligned_labels = torch.tensor(align_labels(word_ids, labels), device=device, dtype=torch.long).unsqueeze(0).repeat(images.size(0),1)
+            aligned_labels = torch.tensor(align_labels(word_ids, labels), device=device, dtype=torch.long).unsqueeze(
+                0).repeat(images.size(0), 1)
             text = {key: value.to(device) for key, value in text.items()}
 
             outputs = model(images, text)
-            loss += loss_criterion(outputs.permute(0, 2, 1), aligned_labels)
-            #y_pred.append(decode_labels_majority_vote(word_ids[1:], torch.argmax(outputs.squeeze(0), dim=1)[1:]))
-            y_pred.append(torch.argmax(outputs, dim=-1)[0:, 1:].tolist())
-            y_true.append(aligned_labels[0:, 1:].tolist())
-            #y_pred.append(decode_labels_majority_vote(word_ids[1:], torch.argmax(outputs, dim=2)[0:, 1:]).tolist())
-            #y_true.append((labels.unsqueeze(0).repeat(outputs.size(0), 1)).tolist())
+            aligned_labels = aligned_labels[0:, 1:-1]
+            outputs = outputs[0:, 1:-1]
+            mask = torch.ones_like(aligned_labels, device=device).bool()
+            loss = model.crf_pass(outputs, aligned_labels, mask, w)
+            # loss = loss_criterion(torch.permute(outputs, (0, 2, 1)), aligned_labels)
+            running_loss += loss.item()
+            y_pred.append(model.crf_decode(outputs, mask))
+            y_true.append(aligned_labels.tolist())
+            """
+            for prediction in list(
+                    map(lambda x: list(map(lambda y: labels_mapping[int(y)], x)), model.crf_decode(outputs,mask))):
+                y_pred.append(prediction)
 
-    metrics = Metrics(y_pred, y_true, 9, {_: str(_) for _ in range(9)})
+            for y_tr in list(
+                    map(lambda batch: list(map(lambda item: labels_mapping[int(item)], batch)), aligned_labels)):
+                y_true.append(y_tr)
+            """
+    metrics = Metrics(y_pred, y_true, len(labels_mapping.keys()), labels_mapping)
     macro_f1 = metrics.macro_f1(metrics.confusion_matrix())
     micro_f1 = metrics.micro_f1(metrics.confusion_matrix())
     acc = metrics.accuracy()
-    return loss / len(validation_data), {"macro": macro_f1, "micro": micro_f1, "accuracy": acc} #(y_true, y_pred)
+
+    #print(classification_report(y_true, y_pred))
+    return running_loss / len(validation_data), {"macro": macro_f1, "micro": micro_f1, "accuracy": acc}  # (y_true, y_pred)
 
 
 def most_common(lst):
     return max(set(lst), key=lst.count)
+
 
 def decode_labels_majority_vote(word_ids, y_pred):
     y_predicted = y_pred.tolist()
@@ -192,8 +283,9 @@ def decode_labels_majority_vote(word_ids, y_pred):
                 current_word_id = w_id
             current_word_labels.append(y_predicted[batch][i])
         decoded_labels.append(most_common(current_word_labels))
-        batched_decoded_labels.append(torch.tensor(decoded_labels, dtype=torch.long,device=device))
+        batched_decoded_labels.append(torch.tensor(decoded_labels, dtype=torch.long, device=device))
     return torch.stack(batched_decoded_labels)
+
 
 def inference_loop_combined_model(model: torch.nn.Module, test_data, tokenizer):
     model.to(device)
