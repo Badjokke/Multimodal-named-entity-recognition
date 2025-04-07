44a0aa8e2923f13945e0157029191812ae1dbe26
jtrefil <jirka.trefilu@gmail.com>
2025-04-07 21:10:33 +0200

    rename convention

--- src/model/language/LstmTokenizer.py
+++ src/model/language/LstmTokenizer.py
@@ -1,6 +1,10 @@
 from typing import Union
+
 import torch
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 class LstmTokenizer:
     def __init__(self, vocabulary: dict[str, int]):
         self.vocabulary = vocabulary
@@ -8,5 +12,5 @@ class LstmTokenizer:
     def __call__(self, *args, **kwargs):
         return self.word_to_vocab_index(args[0])
 
-    def word_to_vocab_index(self, sentence: Union[list[str],tuple[str]]) -> torch.Tensor:
+    def word_to_vocab_index(self, sentence: Union[list[str], tuple[str]]) -> torch.Tensor:
         return torch.tensor(list(map(lambda word: self.vocabulary[word], sentence))).to(device)
